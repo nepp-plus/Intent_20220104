2ee486799e949a21e6c35ae4f54be97f7538dc1f
nepp-plus <nepp.plus@gmail.com>
2022-01-04 10:07:38 +0900

    Intent로 실제 화면 이동

--- app/src/main/java/com/neppplus/intent_20220104/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20220104/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.intent_20220104
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         btnMoveToOther.setOnClickListener {
 
 //            Intent로, 다른 화면으로 이동해보자.
+            val myIntent = Intent( this, OtherActivity::class.java )
+            startActivity(myIntent)
 
         }
 
